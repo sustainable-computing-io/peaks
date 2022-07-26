6367cf32c29434e2b746c82091091aaee7479413
Huamin Chen <rootfs@users.noreply.github.com>
2022-07-26 13:12:33 -0400

    Update energyefficientscheduling.go

--- pkg/energyefficientscheduling/energyefficientscheduling.go
+++ pkg/energyefficientscheduling/energyefficientscheduling.go
@@ -10,26 +10,26 @@ import (
 
 const Name = "EnergyEfficientScheduling"
 
-type Epic struct{}
+type Peaks struct{}
 
-var _ framework.ScorePlugin = &Epic{}
+var _ framework.ScorePlugin = &Peaks{}
 
-func (pl *Epic) Name() string {
+func (pl *Peaks) Name() string {
 	return Name
 }
 
 func New(_ runtime.Object, _ framework.Handle) (framework.Plugin, error) {
-	return &Epic{}, nil
+	return &Peaks{}, nil
 }
 
-func (pl *Epic) Score(ctx context.Context, state *framework.CycleState, pod *v1.Pod, nodeName string) (int64, *framework.Status) {
+func (pl *Peaks) Score(ctx context.Context, state *framework.CycleState, pod *v1.Pod, nodeName string) (int64, *framework.Status) {
 	return 0, nil
 }
 
-func (pl *Epic) ScoreExtensions() framework.ScoreExtensions {
+func (pl *Peaks) ScoreExtensions() framework.ScoreExtensions {
 	return pl
 }
 
-func (pl *Epic) NormalizeScore(ctx context.Context, state *framework.CycleState, pod *v1.Pod, scores framework.NodeScoreList) *framework.Status {
+func (pl *Peaks) NormalizeScore(ctx context.Context, state *framework.CycleState, pod *v1.Pod, scores framework.NodeScoreList) *framework.Status {
 	return nil
 }
