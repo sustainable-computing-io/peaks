b0d4cfa4822a9598bfea3713014a2ef776806965
Felix George <Felix.George@ibm.com>
2023-11-07 00:14:48 +0530

    [PEAKS] Added argyment struct for peaks

---
+++ apis/config/types.go
@@ -0,0 +1,12 @@
+package config
+
+import (
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+)
+
+// +k8s:deepcopy-gen:interfaces=k8s.io/apimachinery/pkg/runtime.Object
+
+type PeaksArgs struct {
+	metav1.TypeMeta
+	WatcherAddress string
+}
